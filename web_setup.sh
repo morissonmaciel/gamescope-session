cfbd27bc119b2f7fd3688102cf32e8a0bdd93050
Morisson Marcel <@morissonmaciel>
2024-02-06 16:47:26 -0300

    Improved setup download experience - Closes #10

---
+++ web_setup.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# Check if git is installed
+if ! command -v git &> /dev/null; then
+    echo "Git is not installed. Installing..."
+    # Install git
+    if ! sudo zypper in git-core; then
+        echo "Failed to install Git."
+        exit 1
+    fi
+fi
+
+# Define repository URL
+repo_url="https://github.com/morissonmaciel/gamescope-session.git"
+
+# Clone the repository into $HOME/.gamescope-setup
+if ! git clone "$repo_url" "$HOME/.gamescope-setup"; then
+    echo "Cloning failed. Downloading zip archive..."
+    # Download zip archive
+    if ! wget -O "$HOME/.gamescope-setup/archive.zip" "https://github.com/morissonmaciel/gamescope-session/archive/refs/tags/v0.10.zip"; then
+        echo "Failed to download zip archive."
+        exit 1
+    fi
+    # Unzip the archive
+    if ! unzip "$HOME/.gamescope-setup/archive.zip" -d "$HOME/.gamescope-setup"; then
+        echo "Failed to unzip archive."
+        exit 1
+    fi
+    # Clean up the zip file
+    if ! rm "$HOME/.gamescope-setup/archive.zip"; then
+        echo "Failed to clean up zip file."
+        exit 1
+    fi
+fi
+
+# Navigate into the cloned repository folder
+if ! cd "$HOME/.gamescope-setup"; then
+    echo "Failed to navigate to the cloned repository folder."
+    exit 1
+fi
+
+# Execute setup.sh script as root
+sudo bash setup.sh
